1ee4bb3c495e6d7bd46917bd8f6278822fc6b4bc
ferz174 <ferzserver@mail.ru>
2020-11-16 15:08:53 +0500

    decodeURI Context params

--- models/Context.js
+++ models/Context.js
@@ -29,12 +29,12 @@ function Context(path, page, app, req, res) {
   this.params = {};
   for(var q in req.query) {
     if (req.query.hasOwnProperty(q)) {
-      this.params[q] = req.query[q];
+      this.params[q] = decodeURI(req.query[q]);
     }
   }
   for(var b in req.body) {
     if (req.body.hasOwnProperty(b)) {
-      this.params[b] = req.body[b];
+      this.params[b] = decodeURI(req.body[b]);
     }
   }
   this.request = this.params.request || path.request || page.item.defaultrequest || "";
@@ -317,6 +317,7 @@ Context.prototype.getParam = function(paramName, defaultValue) {
 };
 
 Context.prototype.setParam = function(paramName, value) {
+	consolw.log(value);
   this.params[paramName] = value;
 };
 
